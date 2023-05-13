0ce33ffa84c168b97689d56ac217be9f60273859
mitruhaa <charonpersonal@proton.me>
2023-05-14 00:39:55 +0300

    add question and choice models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=50)
+    pub_date = models.DateTimeField("date published")
+    question_desctiption = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=3)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
 
 # Create your models here.
